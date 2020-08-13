bf761e99b10f8670908a44cebb63caa5db42325c
VACEfron <46462862+VACEfron@users.noreply.github.com>
2020-08-13 20:18:20 +0200

    Update Objects.cs

--- src/Objects.cs
+++ src/Objects.cs
@@ -4,13 +4,13 @@
     {
         public string Username { get; set; }
         public string AvatarUrl { get; set; }
-        public string CustomBackground { get; set; }
+        public string CustomBackgroundUrl { get; set; }
         public int Level { get; set; }
         public int Rank { get; set; }
         public int CurrentXp { get; set; }
         public int NextLevelXp { get; set; }
         public int PreviousLevelXp { get; set; }
-        public string XpColor { get; set; }
+        public string XpColorHex { get; set; }
         public bool IsBoosting { get; set; }
     }
 }
