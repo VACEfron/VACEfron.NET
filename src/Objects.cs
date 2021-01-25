4e86bff2465ca18943db7fe81f85f8a36b3d4b57
VAC Efron <hayk@live.nl>
2021-01-25 14:08:42 +0100

    Update Objects.cs

--- src/Objects.cs
+++ src/Objects.cs
@@ -5,8 +5,8 @@
         public string Username { get; set; }
         public string AvatarUrl { get; set; }
         public string CustomBackgroundUrl { get; set; }
-        public int Level { get; set; }
-        public int Rank { get; set; }
+        public int? Level { get; set; }
+        public int? Rank { get; set; }
         public int CurrentXp { get; set; }
         public int NextLevelXp { get; set; }
         public int PreviousLevelXp { get; set; }
